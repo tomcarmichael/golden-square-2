52145bb912df56c2d74653c2d57005efaa841301
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-03-15 14:21:21 +0000

    Complete testing and implementation of make_snippet

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,3 +1,8 @@
 def make_snippet(string)
-  return string
+  if string.length <= 5
+    return string
+  else
+    first_5_words = string.split[0..4].join(' ')
+    return "#{first_5_words}..."
+  end
 end
--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -1,7 +1,10 @@
 require "make_snippet"
 
-RSpec.describe "make_snippet mathod" do
+RSpec.describe "make_snippet method" do
   it "for a one word string it returns that string" do
     expect(make_snippet("hello")).to eq "hello"
+  end
+  it "returns the first five words of a string plus '...'" do
+    expect(make_snippet("hello, world, is it me you're looking for?")).to eq "hello, world, is it me..."
   end
 end
