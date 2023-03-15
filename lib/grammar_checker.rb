34addb5942ad26e767b1077f4dbeaad8decfb879
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-03-15 16:46:45 +0000

    Revise grammar_checker to use Regexp and pass all tests

--- lib/grammar_checker.rb
+++ lib/grammar_checker.rb
@@ -1,7 +1,8 @@
 def check_grammar(text)
   first_char = text.chars.first
   last_char = text.chars.last
-  if first_char == first_char.upcase && (last_char == '.' || last_char == '?' || last_char == '!')
+  # if first_char == first_char.upcase && (last_char == '.' || last_char == '?' || last_char == '!')
+  if (first_char.match(/[A-Z]/)) && (last_char.match(/[!?.]/))
     true
   else
     false
