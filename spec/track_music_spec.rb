4bb98ce39bab28bc54de25dd5bba62c63a52fa63
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-03-17 17:48:02 +0000

    Create suite of tests

---
+++ spec/track_music_spec.rb
@@ -0,0 +1,40 @@
+require "track_music"
+
+RSpec.describe MyTracks do
+  
+  it "returns empty array if nothing has been added" do
+    favs = MyTracks.new
+    expect(favs.list).to eq []
+  end
+
+  context "returns tracks added to list" do
+    xit "returns 1 track" do
+      favs = MyTracks.new
+      favs.add("lean on me")
+      expect(favs.list).to eq ["lean on me"]
+    end
+
+    xit "returns 2 tracks" do
+      favs = MyTracks.new
+      favs.add("lean on me", "still T.D.D")
+      expect(favs.list).to eq ["lean on me", "still T.D.D"]
+    end
+  end
+
+  context "Throws an error" do
+    xit "returns error if empty string" do
+      favs = MyTracks.new
+      expect { favs.add("") }.to raise_error "Can't save empty string"
+    end
+
+    xit "returns error if given integer" do
+      favs = MyTracks.new
+      expect { favs.add(2) }.to raise_error "Must be a string"
+    end
+
+    xit "returns error if given nil" do
+      favs = MyTracks.new
+      expect { favs.add }.to raise_error "Must be a string"
+    end
+  end
+end
