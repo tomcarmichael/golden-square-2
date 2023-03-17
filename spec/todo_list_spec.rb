8b3d360f3ebb2e5dd9c5e226ec059f71c8a74950
Jack <jwleggett@outlook.com>
2023-03-17 16:50:08 +0000

    todo_list_spec

---
+++ spec/todo_list_spec.rb
@@ -0,0 +1,42 @@
+require 'todo_list'
+
+RSpec.describe ToDoList do
+
+
+  context "when an item is added" do
+    it "returns that item" do
+      list = ToDoList.new
+      list.add("Walk the dog")
+      expect(list.list_tasks).to eq ["Walk the dog"]
+    end
+  end
+
+
+  it "when no ietms have been added it returns an empty list" do
+    list = ToDoList.new
+    expect(list.list_tasks).to eq []
+  end
+
+
+    it "raises an error when adding an empty string" do
+      list = ToDoList.new
+      expect(list.add("")).to rasie_error "Can't add empty string"
+    end
+
+
+  describe "#complete" do
+    it "removes a completed task from the list" do
+      list = ToDoList.new
+      list.add("Walk the dog")
+      list.complete("Walk the dog")
+      expect.(list.list_tasks).to eq []
+    end
+  end
+
+  it "adds multiple items to the list" do
+    list = ToDoList.new
+    list.add("Walk the dog")
+    list.add("Walk the cat")
+    expect(list.list_tasks).to eq ["Walk the dog", "Walk the cat"]
+  end
+end
